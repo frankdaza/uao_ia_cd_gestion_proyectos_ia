cd9c5f3e047f2c871a669503b0e2d0d2820bafb2
jennramos87 <jennifer.ramos87@gmail.com>
2026-05-26 12:35:31 -0500

    Quité librerias en desuso en data.py

--- src/data.py
+++ src/data.py
@@ -3,12 +3,6 @@
 import pandas as pd
 from config import RAW_DATA_DIR, PROCESSED_DATA_DIR, RAW_DATA_PATH, PROCESSED_DATA_PATH
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import glob
-import os
-
-
 
 def load_raw_data() -> pd.DataFrame:
     if not RAW_DATA_DIR.exists():
---
+++ src/featuring.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from config import RAW_DATA_DIR, PROCESSED_DATA_DIR, RAW_DATA_PATH, PROCESSED_DATA_PATH
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import glob
+import os
